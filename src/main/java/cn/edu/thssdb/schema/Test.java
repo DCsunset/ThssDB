9c119617b9c3eb33f5ce1027526d815de42cfc0f
worsecoder <linfy17@outlook.com>
2020-05-08 10:31:14 +0800

    test(schema): add test for database and table creation

--- src/main/java/cn/edu/thssdb/schema/Test.java
+++ src/main/java/cn/edu/thssdb/schema/Test.java
@@ -7,13 +7,23 @@ import cn.edu.thssdb.storage.*;
 public class Test {
     public static void main(String[] args) {
         Database db = new Database("test");
-        Column[] columns = { new Column("name", ColumnInfo.ColumnType.STRING, false, true, 100) };
-        db.create("tb1", columns);
+        // Column[] columns = { new Column("height", ColumnInfo.ColumnType.INT, false,
+        // true) };
+        // db.create("tb1", columns);
+        // db.create("tb2", columns);
+        // db.quit();
         HashMap<String, Table> tables = db.getTables();
         Table tb = tables.get("tb1");
         System.out.println(tb.tableName);
         Metadata metadata = tb.getMetadata();
         System.out.println(metadata.getRowSize());
         System.out.println(metadata.columns[0]);
+
+        Table tb2 = tables.get("tb2");
+        System.out.println(tb2.tableName);
+        Metadata metadata1 = tb2.getMetadata();
+        System.out.println(metadata1.getRowSize());
+        System.out.println(metadata1.columns[0]);
+        db.quit();
     }
 }
