37edead1ad62cf5d3f65a141e89e04ae2998db12
worsecoder <linfy17@outlook.com>
2020-05-20 17:03:23 +0800

    feat: add multiplecondition

---
+++ src/main/java/cn/edu/thssdb/schema/MultipleCondition.java
@@ -0,0 +1,70 @@
+package cn.edu.thssdb.schema;
+
+import java.util.ArrayList;
+import cn.edu.thssdb.schema.*;
+import cn.edu.thssdb.utils.Global.OpType;
+import cn.edu.thssdb.parser.SQLParser.ComparatorContext;
+import cn.edu.thssdb.parser.SQLParser.ConditionContext;
+import cn.edu.thssdb.parser.SQLParser.Multiple_conditionContext;
+
+public class MultipleCondition {
+    public ArrayList<ArrayList<Condition>> conditions = new ArrayList<ArrayList<Condition>>();
+    private AbstractTable table;
+
+    public MultipleCondition(AbstractTable table, Multiple_conditionContext ctx) {
+        this.table = table;
+        parseCtx(ctx);
+
+    }
+
+    public static OpType ctxtotype(ComparatorContext opCtx) {
+        OpType type = OpType.EQ;
+        if (opCtx.EQ() != null) {
+            type = OpType.EQ;
+        } else if (opCtx.GE() != null) {
+            type = OpType.GE;
+        } else if (opCtx.GT() != null) {
+            type = OpType.GT;
+        } else if (opCtx.LE() != null) {
+            type = OpType.LE;
+        } else if (opCtx.LT() != null) {
+            type = OpType.LT;
+        }
+        return type;
+    }
+
+    private void parseCtx(Multiple_conditionContext ctx) {
+        if (ctx.OR() == null) { // terminal node
+            conditions.add(new ArrayList<Condition>());
+            parseTerminalNode(ctx);
+        } else {
+            parseCtx(ctx.multiple_condition(0));
+            parseCtx(ctx.multiple_condition(1));
+        }
+    }
+
+    private void parseTerminalNode(Multiple_conditionContext ctx) {
+        if (ctx.AND() == null) {
+            ConditionContext cctx = ctx.condition();
+            Condition condition = new Condition(table, cctx.expression().get(0).getText(), ctxtotype(cctx.comparator()),
+                    cctx.expression().get(1).getText());
+            conditions.get(conditions.size() - 1).add(condition);
+        } else {
+            parseTerminalNode(ctx.multiple_condition(0));
+            parseTerminalNode(ctx.multiple_condition(1));
+        }
+    }
+
+    public boolean satisfy(Row row) {
+        for (ArrayList<Condition> andConditions : this.conditions) {
+            boolean result = true;
+            for (Condition condition : andConditions) {
+                result = result && condition.satisfy(row);
+            }
+            if (result) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/cn/edu/thssdb/schema/MultipleConditionTest.java
@@ -0,0 +1,22 @@
+package cn.edu.thssdb.schema;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import cn.edu.thssdb.parser.SQLLexer;
+import cn.edu.thssdb.parser.SQLParser;
+import cn.edu.thssdb.parser.SQLParser.Multiple_conditionContext;
+import cn.edu.thssdb.parser.SQLParser.ParseContext;
+
+public class MultipleConditionTest {
+    public static void main(String[] args) {
+        String str = "select * from table where id=1 && name='hello' || password='c' && q=1234 || p='1212'";
+
+        SQLLexer lexer = new SQLLexer(CharStreams.fromString(str));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SQLParser parser = new SQLParser(tokens);
+        ParseContext root = parser.parse();
+        Multiple_conditionContext ctx = root.sql_stmt_list().sql_stmt().get(0).select_stmt().multiple_condition();
+        MultipleCondition mc = new MultipleCondition(null, ctx);
+    }
+}
