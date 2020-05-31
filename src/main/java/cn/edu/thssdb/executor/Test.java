805c122458ba680ba5701b558deb2a4b3b4226c9
DCsunset <dcsunset@protonmail.com>
2020-05-31 07:01:16 -0700

    test: add test for recovering

--- src/main/java/cn/edu/thssdb/executor/Test.java
+++ src/main/java/cn/edu/thssdb/executor/Test.java
@@ -8,8 +8,12 @@ public class Test {
         manager.createDatabaseIfNotExists("db1");
         manager.switchDatabase("db1");
         SQLExecutor exec = new SQLExecutor();
+        /*
         exec.execute("begin transaction;" + "create TABLE person (name String(256), id Int not null, PRIMARY KEY(ID));"
                 + "insert into person values('test-1', 1);" + "insert into person values('hello', 2);"
                 + "delete from person where name='test-1';" + "update person set name='world' where id=2;" + "commit;");
+
+         */
+        exec.execute("select * from person;");
     }
 }
