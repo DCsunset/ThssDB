a4363c34c2e613cf666f9678bad746325b4d4aac
DCsunset <dcsunset@protonmail.com>
2020-06-11 02:48:52 -0700

    feat: add Client.java

--- src/main/java/cn/edu/thssdb/client/Client.java
+++ src/main/java/cn/edu/thssdb/client/Client.java
@@ -2,194 +2,135 @@ package cn.edu.thssdb.client;
 
 import cn.edu.thssdb.rpc.thrift.ConnectReq;
 import cn.edu.thssdb.rpc.thrift.ConnectResp;
-import cn.edu.thssdb.rpc.thrift.DisconnetReq;
-import cn.edu.thssdb.rpc.thrift.GetTimeReq;
+import cn.edu.thssdb.rpc.thrift.DisconnectReq;
+import cn.edu.thssdb.rpc.thrift.DisconnectResp;
+import cn.edu.thssdb.rpc.thrift.ExecuteStatementReq;
+import cn.edu.thssdb.rpc.thrift.ExecuteStatementResp;
 import cn.edu.thssdb.rpc.thrift.IService;
 import cn.edu.thssdb.utils.Global;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
-import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.PrintStream;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Client {
 
   private static final Logger logger = LoggerFactory.getLogger(Client.class);
 
-  static final String HOST_ARGS = "h";
-  static final String HOST_NAME = "host";
-
-  static final String HELP_ARGS = "help";
-  static final String HELP_NAME = "help";
-
-  static final String PORT_ARGS = "p";
-  static final String PORT_NAME = "port";
-
   private static final PrintStream SCREEN_PRINTER = new PrintStream(System.out);
-  private static final Scanner SCANNER = new Scanner(System.in);
 
   private static TTransport transport;
   private static TProtocol protocol;
   private static IService.Client client;
-  private static CommandLine commandLine;
 
   public static void main(String[] args) {
-    long sessionId = -1;
-    commandLine = parseCmd(args);
-    if (commandLine.hasOption(HELP_ARGS)) {
-      showHelp();
-      return;
-    }
     try {
-      echoStarting();
-      String host = commandLine.getOptionValue(HOST_ARGS, Global.DEFAULT_SERVER_HOST);
-      int port = Integer.parseInt(commandLine.getOptionValue(PORT_ARGS, String.valueOf(Global.DEFAULT_SERVER_PORT)));
-      transport = new TSocket(host, port);
+      transport = new TSocket(Global.DEFAULT_SERVER_HOST, Global.DEFAULT_SERVER_PORT);
       transport.open();
       protocol = new TBinaryProtocol(transport);
       client = new IService.Client(protocol);
-      boolean open = true;
-      while (true) {
-        print(Global.CLI_PREFIX);
-        String msg = SCANNER.nextLine();
 
-        long startTime = System.currentTimeMillis();
-        switch (msg.trim().split(" ")[0]) {
-          case Global.SHOW_TIME:
-            getTime();
-            break;
-          case Global.QUIT:
-            open = false;
-            break;
-          case Global.CONNECT:
-            String[] params = msg.split(" ");
-            if (params.length != 3) {
-              println("Connect <username> <password>");
-              break;
-            }
-            if (sessionId != -1) {
-              println("Please disconnect first!");
-              break;
-            }
-            sessionId = connect(params[1], params[2]);
-            println(String.format("sessionId=%d", sessionId));
-            break;
-          case Global.DISCONNECT:
-            if (sessionId == -1) {
-              println("Cannot disconnect before connect!");
-              break;
-            }
-            disconnect(sessionId);
-            sessionId = -1;
-            break;
-          default:
-            // SQL statement
-            println("Invalid statements!");
-            break;
-        }
-        long endTime = System.currentTimeMillis();
-        println("It costs " + (endTime - startTime) + " ms.");
-        if (!open) {
-          break;
-        }
-      }
+      long sessionId = connect();
+      createDatabase(sessionId);
+      useDatabase(sessionId);
+      createTable(sessionId);
+      insertData(sessionId);
+      queryData(sessionId);
+      disconnect(sessionId);
+
       transport.close();
-    } catch (TTransportException e) {
-      logger.error(e.getMessage());
-    } catch (TException e) {
-
-    }
-  }
-
-  private static void disconnect(long id) {
-    DisconnetReq req = new DisconnetReq();
-    req.sessionId = id;
-    try {
-      client.disconnect(req);
-    } catch (TException e) {
-
-      logger.error(e.getMessage());
-    }
-  }
-
-  private static long connect(String username, String password) {
-    ConnectReq req = new ConnectReq();
-    req.password = password;
-    req.username = username;
-    try {
-      ConnectResp resp = client.connect(req);
-      return resp.sessionId;
-    } catch (TException e) {
-      logger.error(e.getMessage());
-      return -1;
-    }
-  }
-
-  private static void getTime() {
-    GetTimeReq req = new GetTimeReq();
-    try {
-      println(client.getTime(req).getTime());
     } catch (TException e) {
       logger.error(e.getMessage());
     }
   }
 
-  static Options createOptions() {
-    Options options = new Options();
-    options.addOption(Option.builder(HELP_ARGS).argName(HELP_NAME).desc("Display help information(optional)")
-        .hasArg(false).required(false).build());
-    options.addOption(Option.builder(HOST_ARGS).argName(HOST_NAME).desc("Host (optional, default 127.0.0.1)")
-        .hasArg(false).required(false).build());
-    options.addOption(Option.builder(PORT_ARGS).argName(PORT_NAME).desc("Port (optional, default 6667)").hasArg(false)
-        .required(false).build());
-    return options;
-  }
-
-  static CommandLine parseCmd(String[] args) {
-    Options options = createOptions();
-    CommandLineParser parser = new DefaultParser();
-    CommandLine cmd = null;
-    try {
-      cmd = parser.parse(options, args);
-    } catch (ParseException e) {
-      logger.error(e.getMessage());
-      println("Invalid command line argument!");
-      System.exit(-1);
+  private static long connect() throws TException {
+    String username = "username";
+    String password = "password";
+    ConnectReq req = new ConnectReq(username, password);
+    ConnectResp resp = client.connect(req);
+    if (resp.getStatus().code == Global.SUCCESS_CODE) {
+      println("Connect Successfully!");
     }
-    return cmd;
+    return resp.getSessionId();
   }
 
-  static void showHelp() {
-    // TODO
-    println("DO IT YOURSELF");
+  private static void createDatabase(long sessionId) throws TException {
+    String statement = "create database test;";
+    ExecuteStatementReq req = new ExecuteStatementReq(sessionId, statement);
+    ExecuteStatementResp resp = client.executeStatement(req);
+    if (resp.getStatus().code == Global.SUCCESS_CODE) {
+      println("Create Database Successfully!");
+    }
   }
 
-  static void echoStarting() {
-    println("----------------------");
-    println("Starting ThssDB Client");
-    println("----------------------");
+  private static void useDatabase(long sessionId) throws TException {
+    String statement = "use test;";
+    ExecuteStatementReq req = new ExecuteStatementReq(sessionId, statement);
+    ExecuteStatementResp resp = client.executeStatement(req);
+    if (resp.getStatus().code == Global.SUCCESS_CODE) {
+      println("Use Database Successfully!");
+    }
   }
 
-  static void print(String msg) {
-    SCREEN_PRINTER.print(msg);
+  private static void createTable(long sessionId) throws TException {
+    String statement = "create table person (name String(256), ID Int not null, primary key(ID));";
+    ExecuteStatementReq req = new ExecuteStatementReq(sessionId, statement);
+    ExecuteStatementResp resp = client.executeStatement(req);
+    if (resp.getStatus().code == Global.SUCCESS_CODE) {
+      println("Create Table Successfully!");
+    }
   }
 
-  static void println() {
-    SCREEN_PRINTER.println();
+  private static void insertData(long sessionId) throws TException {
+    long startTime = System.currentTimeMillis();
+    String[] statements = {"insert into person values ('Anna', 20);",
+        "insert into person values ('Bob', 22);",
+        "insert into person values ('Cindy', 30);"};
+    for (String statement : statements) {
+      ExecuteStatementReq req = new ExecuteStatementReq(sessionId, statement);
+      ExecuteStatementResp resp = client.executeStatement(req);
+      if (resp.getStatus().code == Global.SUCCESS_CODE) {
+        println("Insert Data Successfully!");
+      }
+    }
+    println("It costs " + (System.currentTimeMillis() - startTime) + "ms.");
+  }
+
+  private static void queryData(long sessionId) throws TException {
+    long startTime = System.currentTimeMillis();
+    String statement = "select * from person;";
+    List<String> columnsList = new ArrayList<>(Arrays.asList("name", "ID"));
+    List<List<String>> rowList = new ArrayList<>(Arrays.asList(
+        new ArrayList<>(Arrays.asList("Anna", "20")),
+        new ArrayList<>(Arrays.asList("Bob", "22")),
+        new ArrayList<>(Arrays.asList("Cindy", "30"))
+    ));
+    ExecuteStatementReq req = new ExecuteStatementReq(sessionId, statement);
+    ExecuteStatementResp resp = client.executeStatement(req);
+    if (resp.getStatus().code == Global.SUCCESS_CODE) {
+      println("Query Data Successfully!");
+    }
+    println("It costs " + (System.currentTimeMillis() - startTime) + "ms.");
+    if (resp.getColumnsList().equals(columnsList) && resp.getRowList().equals(rowList)) {
+      println("The Result Set is Correct!");
+    }
+  }
+
+  private static void disconnect(long sessionId) throws TException {
+    DisconnectReq req = new DisconnectReq(sessionId);
+    DisconnectResp resp = client.disconnect(req);
+    if (resp.getStatus().code == Global.SUCCESS_CODE) {
+      println("Disconnect Successfully!");
+    }
   }
 
   static void println(String msg) {
